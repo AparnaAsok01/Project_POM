9872c8ca67c061fd589f1da3648071b339a6abe1
Aparna Asok <150219208+AparnaAsok01@users.noreply.github.com>
2024-02-01 16:15:10 +0530

    test_sauce_demo.py

--- test_sauce_demo.py
+++ test_sauce_demo.py
@@ -1,4 +1,3 @@
-import unittest
 from selenium import webdriver
 
 from pages.cart_page import CartPage
@@ -7,54 +6,56 @@ from pages.checkout_overview_page import CheckoutOverviewPage
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 import time
+import pytest
 
 
-class TestSauceDemo(unittest.TestCase):
-    def test_saucedemo_workflow(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.get("https://www.saucedemo.com/")
-
-        login_page = LoginPage(driver)
-        login_page.enter_username('standard_user')
-        login_page.enter_password('secret_sauce')
-        login_page.click_login()
-
-        self.assertIn('inventory.html', driver.current_url) 
-
-        # to interact with inventory page
-        inventory_page = InventoryPage(driver)
-        inventory_page.add_to_cart()
-        inventory_page.go_to_cart()
-        time.sleep(5)
-
-        cart_page = CartPage(driver)
-        # time.sleep(5)
-
-        cart_page.proceed_to_checkout()
-
-        checkout_info_page = CheckInfoPage(driver)
-        checkout_info_page.enter_personal_info("john", "wesley", "12345")
-
-        print(driver.current_url)
-
-        self.assertIn('checkout-step-one.html', driver.current_url)
-
-        checkout_info_page.click_continue()
-
-        print(driver.current_url)
-
-        time.sleep(2)
-
-        checkout_overview_page = CheckoutOverviewPage(driver)
-        checkout_overview_page.click_finish()
-
-        time.sleep(5)
-
-        driver.quit()
+@pytest.fixture(scope="module")
+def browser():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver   
+    driver.quit()
 
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_saucedemo_workflow(browser):
+    browser.get("https://www.saucedemo.com/")
+
+    login_page = LoginPage(browser)
+    login_page.enter_username('standard_user')
+    login_page.enter_password('secret_sauce')
+    login_page.click_login()
+
+    assert 'inventory.html' in browser.current_url  
+
+    inventory_page = InventoryPage(browser)
+    inventory_page.add_to_cart()
+    inventory_page.go_to_cart()
+    time.sleep(5)
+
+    cart_page = CartPage(browser)
+    
+
+    cart_page.proceed_to_checkout()
+
+    checkout_info_page = CheckInfoPage(browser)
+    checkout_info_page.enter_personal_info("john", "wesley", "12345")
+
+    print(browser.current_url)
+
+    assert 'checkout-step-one.html' in browser.current_url
+
+checkout_info_page.click_continue()
+
+    print(browser.current_url)
+
+    time.sleep(2)
+
+    checkout_overview_page = CheckoutOverviewPage(browser)
+    checkout_overview_page.click_finish()
+
+    time.sleep(5)
+
+#     browser.quit()
 
 
